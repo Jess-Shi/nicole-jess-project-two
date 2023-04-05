9642fff4ebfe43b8759d9fc866f0d8885cc5adbd
Jess Shi <jess.shi@mail.utoronto.ca>
2023-04-05 12:35:03 -0400

    Made adding event listeners a loop

--- script/script.js
+++ script/script.js
@@ -1,48 +1,19 @@
 
 
-// const footerNav = document.querySelectorAll("footer nav");
+const footerNav = document.querySelectorAll(".useful-links, .account, .services, .questions");
 
-// for (let i = 0; i < footerNav.length; i++) {
-//     footerNav[i].classList.toggle("display");
-// }
+for (let i = 0; i < footerNav.length; i++) {
+    
+    footerNav[i].addEventListener("click", () => {
 
-// const myAccount = document.querySelector(".account ul");
-
-// console.log(myAccount);
-
-// document.querySelector(".account").addEventListener("click", () => {
-//     document.querySelector(".account ul").classList.toggle("display");
-// });
-
-
-// myAccount.classList.toggle("display");
-
-const display = (selector) => {
-    document.querySelector(selector).classList.toggle("display");
+        const menu = document.querySelector(`.${footerNav[i].classList[1]} ul`);
+        menu.classList.toggle("display");
+        menu.scrollIntoView({ behavior: "smooth" });
+        
+        if (i === 3) {
+            document.querySelector(`.${footerNav[i].classList[1]} p`).classList.toggle("display");
+        }
+    })
 }
 
-const scrollIntoView = (selector) => {
-    document.querySelector(selector).scrollIntoView({behavior: "smooth"});
-}
-
-document.querySelector(".useful-links").addEventListener("click", () => {
-    display(".useful-links ul");
-    scrollIntoView(".useful-links ul");
-});
-
-document.querySelector(".account").addEventListener("click", () => {
-    display(".account ul");
-    scrollIntoView(".account ul");
-});
-
-document.querySelector(".services").addEventListener("click", () => {
-    display(".services ul");
-    scrollIntoView(".services ul");
-});
-
-document.querySelector(".questions").addEventListener("click", () => {
-    display(".questions ul");
-    scrollIntoView(".questions ul");
-    display(".questions p");
-});
 
