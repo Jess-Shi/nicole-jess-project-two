1415a7112b7fe206963a6506876a390177be96c8
Jess Shi <jess.shi@mail.utoronto.ca>
2023-04-20 22:09:19 -0400

    More bug fixes for Safari

--- script/script.js
+++ script/script.js
@@ -19,10 +19,10 @@ onValue(dbRef, (data) => {
   const products = ourData.products;
   const cart = ourData.cart;
 
-  if (document.querySelector(".featured")) {
-    displayItems(products);
-  }
+  // if (document.querySelector(".featured")) {
+  // }
   
+  displayItems(products);
   updateCartCount(cart);
 });
 
